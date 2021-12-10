e0fdaac65e37638dcb3c61d26cd814632d135c45
enescva <87376170+enescva@users.noreply.github.com>
2021-12-10 22:26:09 +0100

    Create pull_projects_cb.sh

---
+++ pull_projects_cb.sh
@@ -0,0 +1,28 @@
+REM 
+git clone --branch master https://github.com/openefsa/catalogue-browser.git
+REM 
+git clone --branch master https://github.com/openefsa/catalogue-xml-to-xlsx.git
+REM proxy-auth branch
+git clone --branch proxy-auth https://github.com/openefsa/dcf-webservice-framework.git
+REM 
+git clone --branch master https://github.com/openefsa/exceptions-manager.git
+REM 
+git clone --branch master https://github.com/openefsa/external-installer.git
+REM proxy-auth branch
+git clone --branch proxy-auth https://github.com/openefsa/http-manager.git
+REM 
+git clone --branch master https://github.com/openefsa/http-manager-gui.git
+REM 
+git clone --branch master https://github.com/openefsa/open-xml-reader.git
+REM 
+git clone --branch master https://github.com/openefsa/progress-bar.git
+REM 
+git clone --branch master https://github.com/openefsa/sql-script-executor.git
+REM 
+git clone --branch master https://github.com/openefsa/version-manager.git
+REM 
+git clone --branch master https://github.com/openefsa/window-size-save-restore.git
+REM 
+git clone --branch master https://github.com/openefsa/zip-manager.git
+REM 
+git clone --branch master https://github.com/openefsa/interpreting-and-checking-tool.git
